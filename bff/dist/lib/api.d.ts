daa48e3e994268de204c0371c44aeb20be59301a
ARAKI, Shin'ichi <console.0.25.0.1@gmail.com>
2026-04-01 17:15:45 +0900

    bff/distを削除

--- bff/dist/lib/api.d.ts
+++
@@ -1,4 +0,0 @@
-import type { paths } from "../types/api.d.ts";
-declare const client: import("openapi-fetch").Client<paths, `${string}/${string}`>;
-export default client;
-//# sourceMappingURL=api.d.ts.map
